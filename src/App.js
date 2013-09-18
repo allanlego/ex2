fd2e97946eee1a34c7e73591434222e4bad6f84e
Allan Lykke Christensen <allan.lykke.christensen@lego.com>
2013-09-18 10:51:47 -0400

    Initial import

---
+++ src/App.js
@@ -0,0 +1,23 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+  items:{
+        html:'<a href="https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.ui.cardboard.CardBoard">Hint!<a>'
+    },
+
+                launch: function() {
+                    var cardBoardConfig = {
+                        xtype: 'rallycardboard',
+                        types: ['User Story'],
+                        attribute: "ScheduleState",
+                        cardConfig: {
+                            cls: 'Panda',
+                            xtype: 'customcard'
+                        }
+                    };
+            
+                    this.add(cardBoardConfig);
+                }
+});
+
+
